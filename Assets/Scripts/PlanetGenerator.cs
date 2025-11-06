892d2b0aae64f1aef66f527b6b8090950ae97767
Dominik Thomas <37384697+DominikThomas@users.noreply.github.com>
2025-11-06 19:44:47 +0100

    Adding initial files for generating a sphere

---
+++ Assets/Scripts/PlanetGenerator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlanetGenerator : MonoBehaviour // <--- This tells Unity it is a component!
+{
+    // Standard Unity method called when the script starts
+    void Start()
+    {
+        // Your code will go here
+    }
+
+    // Standard Unity method called every frame
+    void Update()
+    {
+        // ...
+    }
+}
